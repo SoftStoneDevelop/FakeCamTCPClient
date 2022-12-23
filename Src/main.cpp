9de7b579844ef1b24146f4c21657c769e1c42072
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-12-23 21:09:45 +0300

    init commit

---
+++ Src/main.cpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+
+#include "ArrayPool.hpp"
+#include "MemoryOwnerFactory.hpp"
+#include "TCPClient.hpp"
+
+int main(int argc, char* argv[])
+{
+    auto pool = std::make_shared<ArrayPool::ArrayPool<char>>();
+    ArrayPool::MemoryOwnerFactory<char> mof(std::move(pool));
+    FakeCamTCPClient::TCPClient client(
+        "127.0.0.1",
+        "4823",
+        mof
+    );
+
+    while (!client.Connect())
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+
+    auto responce = mof.rentMemory(100);
+    std::string command;
+    int commandSize = 0;
+    while (true)
+    {
+        std::cout << "Enter command:" << std::endl;
+        std::getline(std::cin, command);
+        client.sendCommand(command.data(), command.size(), responce);
+        std::cout << responce.data();
+    }
+
+    return EXIT_SUCCESS;
+}
